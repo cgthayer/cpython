a16c2eeb14fce560f500f3c069a09a2dd0e30bf3
Charles Thayer <cgthayer@users.noreply.github.com>
2019-09-30 12:32:00 -0700

    Just kidding
    ```
    import random
    import this
    print(random.choice(this.a))
    ```

--- Lib/this.py
+++ Lib/this.py
@@ -25,4 +25,6 @@ for c in (65, 97):
     for i in range(26):
         d[chr(i+c)] = chr((i+13) % 26 + c)
 
-print("".join([d.get(c, c) for c in s]))
+t = "".join([d.get(c, c) for c in s])
+print(t)
+a = t.split("\n")[2:]
